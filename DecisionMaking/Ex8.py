1729f70371aeed1a4f051b3703a5751d3b380152
Duyha2000 <duytd19405c@st.uel.edu.vn>
2024-03-01 21:53:57 +0700

    If - else, switch - case

--- DecisionMaking/Ex8.py
+++ DecisionMaking/Ex8.py
@@ -1,18 +1,20 @@
 month = int(input("Enter month: "))
 year = int(input("Enter year: "))
-match month:
-    case 1, 3, 5, 7, 8, 10, 12:
-        print("31 days")
-    case 4, 6, 9, 11:
-        print("30 days")
-    case 2:
-        if year % 4 == 0:
-            if year % 100 == 0:
-                if year % 400 == 0:
-                    print("29 days")
-                else:
-                    print("28 days")
-            else:
+
+if month == 2:
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
                 print("29 days")
-    case _:
-        print("Invalid month")
+            else:
+                print("28 days")
+        else:
+            print("29 days")
+    else:
+        print("28 days")
+elif month in (1, 3, 5, 7, 8, 10, 12):
+    print("31 days")
+elif month in (4, 6, 9, 11):
+    print("30 days")
+else:
+    print("Invalid month")
